0c10f9a3e46b491f493d9c6fe86e7289a7df2ad4
이장호 <libu1129@naver.com>
2023-05-01 23:15:48 +0900

    중복실행 방지

--- DubuTimeSync/Program.cs
+++ DubuTimeSync/Program.cs
@@ -2,12 +2,16 @@ namespace DubuTimeSync
 {
     internal static class Program
     {
+        private static Mutex mutex = new Mutex(true, "DubuTimeSync");
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            if (!mutex.WaitOne(TimeSpan.Zero, true)) return;
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
